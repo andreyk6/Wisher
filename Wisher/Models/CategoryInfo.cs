4f53f67b47efd02061ae6e27fcc1e802f182d528
andreyk6 <andreyk6@gmail.com>
2015-12-12 18:35:34 +0200

    -Add eBay nuget package
    -Create Singleton EbayApiContext
    -Create CategoryInfo class

---
+++ Wisher/Models/CategoryInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wisher.Models
+{
+    public class CategoryInfo
+    {
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public string PictureUrl { get; set; }
+        public int Level { get; set; }
+
+        public int EbayCategoryId { get; set; }
+        public int EbayParrentCategoryId { get; set; }
+
+        public int Id { get; set; }
+    }
+}
