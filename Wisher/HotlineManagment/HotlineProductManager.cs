d6ceab936efaa1c8cda9ad52a1327a345942696a
andreyk6 <andreyk6@gmail.com>
2015-12-13 06:16:20 +0200

    add product loader from hotline

---
+++ Wisher/HotlineManagment/HotlineProductManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Wisher.Models;
+
+namespace Wisher.HotlineManagment
+{
+    public class HotlineProductManager
+    {
+        public static HotlineProduct GetToProducts(CategoryInfo category, int count = 1)
+        {
+            List<CategoryInfo> categories = new List<CategoryInfo>();
+
+            HtmlWeb web = new HtmlWeb();
+            var document = web.Load(@"http://m.hotline.ua" + category.EbayCategoryId);
+            var firstLevel = ElementsByClass(document, "a", "list_tovar");
+
+            return null;
+        }
+
+        static HtmlNodeCollection ElementsByClass(HtmlDocument doc, string element, string className)
+        {
+            return doc.DocumentNode.SelectNodes("//" + element + "[@class='" + className + "']");
+        }
+    }
+
+    public class HotlineProduct
+    {
+        
+    }
+}
