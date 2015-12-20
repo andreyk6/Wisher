b5621373e833deef83669a008a25b1f05217bc25
Александр Павлыщ <dubli543@gmail.com>
2015-12-20 16:08:23 +0200

    add default function dom manipulation

--- layout/source/js/index.js
+++ layout/source/js/index.js
@@ -1,37 +1,107 @@
 (function () {
-	var $preloader = $('#preloader'),
-		$pageSlider = $('#pageSlider'),
-		$countdown = $('#countdown');
+	var whr = {},
+		hideClass = "hide";
 
-	var onDomReady = function(){
-
-		$pageSlider.fullpage({
-			sectionSelector: '.page-section',
-			slideSelector: '.page-slide',
-			slidesNavigation: false,
-			controlArrows: false,
-			scrollOverflow: true,
-			paddingTop: '81px'
-		});
-
-		// $.fn.fullpage.setKeyboardScrolling(false, 'all');
-		// $.fn.fullpage.setAllowScrolling(false, 'all');
-		// $.fn.fullpage.setRecordHistory(false);
-
-		$countdown.countdown({
-			date: "January 01, 2016 00:00:00",
-			render: function(data) {
-				var el = $(this.el);
-				el.empty()
-				.append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-				.append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-				.append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-				.append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+	whr.fadeOut = function (element, fadeOutDuration) {
+		var op = 1,
+			duration = fadeOutDuration || 50;
+		var timer = setInterval(function () {
+			if (op <= 0.1){
+				clearInterval(timer);
+				element.style.display = 'none';
+				element.classList.add(hideClass);
 			}
-		});
-
-		$preloader.hide();
+			element.style.opacity = op;
+			element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+			op -= op * 0.1;
+		}, duration);
 	};
 
-	onDomReady();
+	whr.fadeIn = function (element, fadeInDuration) {
+		var op = 0.1,
+			duration = fadeInDuration|| 10;
+		element.classList.remove(hideClass);
+		element.style.display = 'block';
+		var timer = setInterval(function () {
+			if (op >= 1){
+				clearInterval(timer);
+			}
+			element.style.opacity = op;
+			element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+			op += op * 0.1;
+		}, duration);
+	};
+
+	whr.hide = function(element){
+		element.classList.add(hideClass);
+	};
+
+	whr.show = function(element){
+		element.classList.remove(hideClass);
+	};
+
+
+
+	var preloader = document.getElementById('preloader');
+
+	whr.hide(preloader)
+	// 	$countdown = $('#countdown');
+
+	fullpage.initialize("#pageSlider", {
+		// 'anchors': ['section1', 'section2', 'section3'],
+		'sectionSelector': '.page-section',
+		'slideSelector': '.page-slide',
+		// 'keyboardScrolling': false,
+		'navigation': false,
+		'recordHistory': false,
+		'slidesNavigation': false,
+		'controlArrows': false,
+		'loopHorizontal': false,
+		'afterLoad': function(i){
+			console.log('afterLoad', i);
+		},
+		'onLeave': function(index, nextIndex, direction){
+			console.log('onLeave', index, nextIndex, direction);
+		},
+		'afterRender': function(i){
+			console.log('afterRender', i);
+		},
+		'afterResize': function(i){
+			console.log('afterResize', i);
+		},
+		'afterReBuild': function(i){
+			console.log('afterReBuild', i);
+		},
+		'afterSlideLoad': function(i){
+			console.log('afterSlideLoad', i);
+		},
+		'onSlideLeave': function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+			console.log('onSlideLeave', anchorLink, index, slideIndex, direction, nextSlideIndex);
+		}	
+	});
+
+	// fullpage.setKeyboardScrolling(false, 'all');
+	// fullpage.setAllowScrolling(false, 'all');
+	fullpage.setRecordHistory(false);
+
+	// var onDomReady = function(){
+
+
+
+	// 	$countdown.countdown({
+	// 		date: "January 01, 2016 00:00:00",
+	// 		render: function(data) {
+	// 			var el = $(this.el);
+	// 			el.empty()
+	// 			.append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+	// 			.append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
+	// 			.append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
+	// 			.append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+	// 		}
+	// 	});
+
+	// 	$preloader.hide();
+	// };
+
+	// onDomReady();
 })();
