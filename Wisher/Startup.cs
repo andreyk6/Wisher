f941246dbf13018d33f5876ee2813ac8ef828f61
Iasko Andrey <a.iasko-2020@kpi.ua>
2015-12-20 23:48:17 +0200

    finish google plus registration/authentication

--- Wisher/Startup.cs
+++ Wisher/Startup.cs
@@ -45,6 +45,9 @@ namespace Wisher
                 AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
                 Provider = new SimpleAuthorizationServerProvider()
             };
+
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(OAuthBearerOptions);
             GoogleAuthOptions = new GoogleOAuth2AuthenticationOptions()
             {
                 ClientId = "655406860655-ikkgjfk17qkppfh6l6on54k52hbflm88.apps.googleusercontent.com",
@@ -61,8 +64,7 @@ namespace Wisher
             };
             app.UseFacebookAuthentication(FacebookAuthOptions);
 
-            app.UseOAuthAuthorizationServer(oAuthServerOptions);
-            app.UseOAuthBearerAuthentication(OAuthBearerOptions);
+            
         }
         private void ConfigureWebApi(HttpConfiguration config)
         {
